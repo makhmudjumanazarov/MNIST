b5af2dd24a0a7827fa6c6fe4e04dfb967462e21b
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-23 16:52:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def full_app():
         stroke_color = st.sidebar.color_picker("Stroke color hex: ")
         bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-        realtime_update = st.sidebar.checkbox("Update in realtime", True)
+#         realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
         # Create a canvas component
         canvas_result = st_canvas(
@@ -51,7 +51,7 @@ def full_app():
             stroke_color=stroke_color,
             background_color=bg_color,
             background_image=Image.open(bg_image) if bg_image else None,
-            update_streamlit=realtime_update,
+#             update_streamlit=realtime_update,
             height=150,
             drawing_mode=drawing_mode,
             point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
