df8450d536fac410adadc5005b5a4d00d4ddcc99
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-23 16:49:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ if __name__ == "__main__":
         page_title="Streamlit Drawable Canvas Demo", page_icon=":pencil2:"
     )
     st.title("Drawable Canvas Demo")
-    st.sidebar.subheader("Configuration")
+#     st.sidebar.subheader("Configuration")
     main()
