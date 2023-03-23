cf14f2f133e261b44d5962d01512031955188a28
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-24 00:36:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,73 +9,52 @@ from streamlit_drawable_canvas import st_canvas
 from svgpathtools import parse_path
 from pathlib import Path
 
-# def main():
-#     if "button_id" not in st.session_state:
-#         st.session_state["button_id"] = ""
-#     if "color_to_label" not in st.session_state:
-#         st.session_state["color_to_label"] = {}
-#     PAGES = {
-#         "Basic example": full_app,
-#     }
-#     page = st.sidebar.selectbox("Page:", options=list(PAGES.keys()))
-#     PAGES[page]()
-# def full_app():
-#     with st.echo("below"):
-#         drawing_mode = st.sidebar.selectbox(
-#             "Drawing tool:",
-#             ("freedraw", "point"),
-#         )
-#         stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-#         if drawing_mode == 'point':
-#             point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-#         stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-#         bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-#         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+def main():
+    if "button_id" not in st.session_state:
+        st.session_state["button_id"] = ""
+    if "color_to_label" not in st.session_state:
+        st.session_state["color_to_label"] = {}
+    PAGES = {
+        "Basic example": full_app,
+    }
+    page = st.sidebar.selectbox("Page:", options=list(PAGES.keys()))
+    PAGES[page]()
+def full_app():
+    with st.echo("below"):
+        drawing_mode = st.sidebar.selectbox(
+            "Drawing tool:",
+            ("freedraw", "point"),
+        )
+        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+        if drawing_mode == 'point':
+            point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+        bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+        bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 
-#         # Create a canvas component
-#         canvas_result = st_canvas(
-#             fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-#             stroke_width=stroke_width,
-#             stroke_color=stroke_color,
-#             background_color=bg_color,
-#             background_image=Image.open(bg_image) if bg_image else None,
-#             height=150,
-#             drawing_mode=drawing_mode,
-#             point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
-#             key="full_app",
-#         )
+        # Create a canvas component
+        canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+            stroke_width=stroke_width,
+            stroke_color=stroke_color,
+            background_color=bg_color,
+            background_image=Image.open(bg_image) if bg_image else None,
+            height=150,
+            drawing_mode=drawing_mode,
+            point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+            key="full_app",
+        )
 
-#         # Do something interesting with the image data and paths
-#         if canvas_result.image_data is not None:
-#             st.image(canvas_result.image_data)
-#         if canvas_result.json_data is not None:
-#             objects = pd.json_normalize(canvas_result.json_data["objects"])
-#             for col in objects.select_dtypes(include=["object"]).columns:
-#                 objects[col] = objects[col].astype("str")
+        # Do something interesting with the image data and paths
+        if canvas_result.image_data is not None:
+            st.image(canvas_result.image_data)
+        if canvas_result.json_data is not None:
+            objects = pd.json_normalize(canvas_result.json_data["objects"])
+            for col in objects.select_dtypes(include=["object"]).columns:
+                objects[col] = objects[col].astype("str")
 #             st.dataframe(objects)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
-# Create a canvas component
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
-    height=150,
-    drawing_mode=drawing_mode,
-    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
-    key="full_app",
-)
-
-# Do something interesting with the image data and paths
-if canvas_result.image_data is not None:
-    st.image(canvas_result.image_data)
-if canvas_result.json_data is not None:
-    objects = pd.json_normalize(canvas_result.json_data["objects"])
-    for col in objects.select_dtypes(include=["object"]).columns:
-        objects[col] = objects[col].astype("str")
-    st.dataframe(objects)
