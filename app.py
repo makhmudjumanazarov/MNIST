2a36e43a026376f923133ed14e628d9c4112d355
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-23 17:12:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -40,12 +40,12 @@ def full_app():
 
         # Create a canvas component
         canvas_result = st_canvas(
-            fill_color="rgba(255, 100, 0, 0.3)",  # Fixed fill color with some opacity
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
             stroke_width=stroke_width,
             stroke_color=stroke_color,
             background_color=bg_color,
             background_image=Image.open(bg_image) if bg_image else None,
-            height=150,
+            height=100,
             drawing_mode=drawing_mode,
             point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
             key="full_app",
