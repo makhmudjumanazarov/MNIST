da93c788e87c638e3c89e4c2abff86bcc4fc81f3
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-24 00:34:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -21,16 +21,16 @@ def main():
     PAGES[page]()
 def full_app():
     with st.echo("below"):
-        drawing_mode = st.sidebar.selectbox(
-            "Drawing tool:",
-            ("freedraw", "point"),
-        )
-        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-        if drawing_mode == 'point':
-            point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-        bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-        bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+#         drawing_mode = st.sidebar.selectbox(
+#             "Drawing tool:",
+#             ("freedraw", "point"),
+#         )
+#         stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+#         if drawing_mode == 'point':
+#             point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+#         stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+#         bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+#         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 
         # Create a canvas component
         canvas_result = st_canvas(
@@ -53,83 +53,7 @@ def full_app():
             for col in objects.select_dtypes(include=["object"]).columns:
                 objects[col] = objects[col].astype("str")
 #             st.dataframe(objects)
-def png_export():
-    st.markdown(
-        """
-    Realtime update is disabled for this demo. 
-    Press the 'Download' button at the bottom of canvas to update exported image.
-    """
-    )
-    try:
-        Path("tmp/").mkdir()
-    except FileExistsError:
-        pass
 
-    # Regular deletion of tmp files
-    # Hopefully callback makes this better
-    now = time.time()
-    N_HOURS_BEFORE_DELETION = 1
-    for f in Path("tmp/").glob("*.png"):
-        st.write(f, os.stat(f).st_mtime, now)
-        if os.stat(f).st_mtime < now - N_HOURS_BEFORE_DELETION * 3600:
-            Path.unlink(f)
-
-    if st.session_state["button_id"] == "":
-        st.session_state["button_id"] = re.sub(
-            "\d+", "", str(uuid.uuid4()).replace("-", "")
-        )
-
-    button_id = st.session_state["button_id"]
-    file_path = f"tmp/{button_id}.png"
-
-    custom_css = f""" 
-        <style>
-            #{button_id} {{
-                display: inline-flex;
-                align-items: center;
-                justify-content: center;
-                background-color: rgb(255, 255, 255);
-                color: rgb(38, 39, 48);
-                padding: .25rem .75rem;
-                position: relative;
-                text-decoration: none;
-                border-radius: 4px;
-                border-width: 1px;
-                border-style: solid;
-                border-color: rgb(230, 234, 241);
-                border-image: initial;
-            }} 
-            #{button_id}:hover {{
-                border-color: rgb(246, 51, 102);
-                color: rgb(246, 51, 102);
-            }}
-            #{button_id}:active {{
-                box-shadow: none;
-                background-color: rgb(246, 51, 102);
-                color: white;
-                }}
-        </style> """
-
-    data = st_canvas(update_streamlit=False, key="png_export")
-    if data is not None and data.image_data is not None:
-        img_data = data.image_data
-        im = Image.fromarray(img_data.astype("uint8"), mode="RGBA")
-        im.save(file_path, "PNG")
-
-        buffered = BytesIO()
-        im.save(buffered, format="PNG")
-        img_data = buffered.getvalue()
-        try:
-            # some strings <-> bytes conversions necessary here
-            b64 = base64.b64encode(img_data.encode()).decode()
-        except AttributeError:
-            b64 = base64.b64encode(img_data).decode()
-
-        dl_link = (
-            custom_css
-            + f'<a download="{file_path}" id="{button_id}" href="data:file/txt;base64,{b64}">Export PNG</a><br></br>'
-        )
-        st.markdown(dl_link, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
