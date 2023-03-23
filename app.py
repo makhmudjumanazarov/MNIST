e072256fb2100b277754277cffaa0a1da66ec09d
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-23 17:00:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,12 +26,10 @@ def main():
     page = st.sidebar.selectbox("Page:", options=list(PAGES.keys()))
     PAGES[page]()
 def full_app():
-
     with st.echo("below"):
-        # Specify canvas parameters in application
         drawing_mode = st.sidebar.selectbox(
             "Drawing tool:",
-            ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+            ("freedraw", "point"),
         )
         stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
         if drawing_mode == 'point':
