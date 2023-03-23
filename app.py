f04d716d5bc79e9612e3dc72ddf025f64a85bc0f
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-23 17:16:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def full_app():
         stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
         if drawing_mode == 'point':
             point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-        bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-        bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+#         stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+#         bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+#         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 
         # Create a canvas component
         canvas_result = st_canvas(
