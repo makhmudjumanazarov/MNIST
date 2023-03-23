b3e97483d3d3c60d2b40625e4b8337159155c88e
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-23 17:09:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def full_app():
         )
 
         # Do something interesting with the image data and paths
-        if canvas_result.image_data is not None:
-            st.image(canvas_result.image_data)
+#         if canvas_result.image_data is not None:
+#             st.image(canvas_result.image_data)
         if canvas_result.json_data is not None:
             objects = pd.json_normalize(canvas_result.json_data["objects"])
             for col in objects.select_dtypes(include=["object"]).columns:
