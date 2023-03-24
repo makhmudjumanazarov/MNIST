e7b803ad80d8a75797b3e366d4c2d17934427b7f
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-24 11:13:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 from svgpathtools import parse_path
 from pathlib import Path
+from tensorflow.keras.models import load_model
 
 def get_images(data, clas):
     return [cv2.imread(i) for i in glob('streamlit_samples/'+data+'/'+clas+'*')]
